c532038ef446766b798afd0b802fd0a3ca3bf29c
jctoapanta <jc_toapanta@yahoo.com>
2016-02-23 15:21:36 -0500

    Translate docpad.js file to spanish and more

--- docpad.js
+++ docpad.js
@@ -2,20 +2,20 @@ module.exports = {
 
   prompts: false,
 
-  // These are variables will be accessible via our templates
+  // Éstas son variables que serán accesibles a través de nuestras plantillas
   templateData: {
 
-    // Conference info
+    // Información de la conferencia
     conf: {
       name: "PythonQuito",
       description: "La Primera Semana Pythónica de Quito",
       date: "4-5 y 8-9 de Marzo 2016",
-      // If your event is free, just comment this line
+      // Si su evento es gratuito, solo comente esta línea
       price: "$0 (DjangoGirls), $10 (Charlas + Camiseta)",
       venue: "",
-      address: "Ladrón de Guevera E11-253, Escuela Politécnica Nacional",
+      address: "Ladrón de Guevara E11-253, Escuela Politécnica Nacional",
       address2: "Edif. de Administración, Hemiciclo Politécnico (Planta Baja)",
-      googleMapsAddress: "Ladron de Guevera E11-253, Quito 170517",
+      googleMapsAddress: "Ladrón de Guevara E11-253, Quito 170517",
       city: "Quito",
       state: "Pichincha, Ecuador"
     },
@@ -24,8 +24,8 @@ module.exports = {
       mail: "pythonquito@googlegroups.com"
     },
 
-    // The Call To Action button at the header,
-    // If you don't want this, just remove the callToAction property.
+    // Botón llamada a Acción en la cabecera
+    // Si no quiere esto, solo remueva la propiedad callToAction
     callToAction: {
         text: "Regístrate Ahora!",
         link: "/inscripcion"
@@ -33,16 +33,16 @@ module.exports = {
 
     propuestasForm: "/propuestas",
 
-    // Site info
+    // Información del sitio
     site: {
       theme: "yellow-swan",
       url: "http://pythonquito.tk/",
       googleanalytics: "UA-73316645-1"
     },
 
-    // Active sections on the website
-    // to deactivate comment out with '//'
-    // you can also change order here and it will reflect on page
+    // Secciones activas en el sitio web
+    // Para desactivar coméntelo con '//'
+    // puede también cambiar el orden aquí y se reflejará en la página
     sections: [
       'about',
       'talks',
@@ -52,74 +52,74 @@ module.exports = {
       'contact'
     ],
 
-    // Labels which you can translate to other languages
+    // Etiquetas que puede traducir a otros lenguajes
     labels: {
       about: "Acerca de",
       talks: "DjangoGirls & Charlas Pythónicas",
       location: "Lugar",
       organizers: "Organizadores",
-      partners: "Auspiciantes y Apoyadores",
+      partners: "Auspiciantes y Colaboradores",
       contact: "Contacto"
     },
 
-    // The entire schedule
+    // Programación completa
     schedule: [
       {
-        name: "Check-in / Breakfast",
+        name: "Registro / Desayuno",
         time: "9h00"
       },
       {
-        name: "Linus Torvalds",
+        name: "Galoget Latorre",
         photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Linux Foundation",
+        bio: "Egresado de la Facultad de Sitemas de la EPN, apasionado por la Seguridad Informática y todo lo que tenga que ver con Tecnología",
+        company: "Comunidad de Software Libre y Seguridad Informática - HACKEM",
         link: {
-          href: "http://twitter.com/linus",
-          text: "@linus"
+          href: "http://twitter.com/Galoget",
+          text: "@Galoget"
         },
         presentation: {
-          title: "Digging into a Linux Kernel",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          title: "Python como herramienta de Hacking",
+          description: "Una introducción sobre el uso de Python como herramienta de Hacking",
           time: "10h00"
         }
       },
       {
-        name: "Bill Gates",
+        name: "Carlos Toapanta",
         photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Microsoft",
+        bio: "Estudiante de nivel superior de la Facultad de Electrónica y Control de la EPN, Hacker Ético",
+        company: "Comunidad de Software Libre y Seguridad Informática - HACKEM",
         link: {
-          href: "http://github.com/billy95",
-          text: "@billy95"
+          href: "http://github.com/cdtoapanta",
+          text: "@cdtoapanta"
         },
         presentation: {
-          title: "Introducing Windows 12",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          title: "SageMath",
+          description: "Una introducción de SageMath como solución libre a MathLab y otras",
           time: "11h00"
         }
       },
       {
-        name: "Lunch",
+        name: "Almuerzo",
         time: "12h00"
       },
       {
-        name: "Chuck Norris",
+        name: "Roberto Armas",
         photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Delta Command",
+        bio: "Estudiante de nivel superior de la UDLA",
+        company: "Comunidad de Software Libre y Seguridad Informática - HACKEM",
         link: {
-          href: "http://twitter.com/littlechuck",
-          text: "@littlechuck"
+          href: "http://twitter.com/hvillavicencio",
+          text: "@hvillavicencio"
         },
         presentation: {
-          title: "How to kill a elephant with one finger",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          title: "Aplicaciones web con Python",
+          description: "Una introducción al desarrollo de aplicaciones android basadas en Python",
           time: "13h00"
         }
       }
     ],
 
-    // List of Partners
+    // Lista de Auspiciantes y Colaboradores
     organizers: [
       {
         name: "Álvaro Justen",
@@ -146,6 +146,16 @@ module.exports = {
         logo: "https://pbs.twimg.com/profile_images/667863177821102080/6wYGuxNX.jpg",
         url: "https://twitter.com/vrokida"
       }
+      {
+        name: "Juan Carlos Toapanta",
+        logo: "https://avatars2.githubusercontent.com/u/16170915?v=3&u=bb6f15bcdb75a60a312239654b309625b5a2a482&s=140",
+        url: "https://github.com/jctoapanta"
+      }
+      {
+        name: "Daniel Toapanta",
+        logo: "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash2/v/t1.0-9/10464182_10153580445312785_1530515005628569746_n.jpg?oh=5782e8f155e7ac6207e13f54e84af4ad&oe=572EAD1D&__gda__=1465779181_092e884bdb5b29846f260a16e38321c4",
+        url: "https://electronicaecuador.blogspot.com"
+      }
     ],
     sponsors: [
       {
@@ -162,13 +172,13 @@ module.exports = {
       }
     ],
 
-    // Theme path
+    // Camino de Directorios de temas
     getTheme: function() {
       return "";
       //return "themes/" + this.site.theme;
     },
 
-    // Site Path
+    // Camino de Directorios del sitio
     getUrl: function() {
       return this.site.url;
     }
